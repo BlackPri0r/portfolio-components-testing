b0b085dab2c6123eb28dbec10d6ff0582ed5c4e3
BlackPri0r <lawrenceli630@gmail.com>
2024-10-20 00:38:56 +1300

    feat: testing/learning interactive cloud component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import React from "react"
+import IconCloud from "./DynamicIconCloud"
+
 
 function App() {
   return (
     <>
       <div>
-        hello
+        test :D
       </div>
+      <IconCloud iconSlugs={
+        ["wix", 
+          "react", 
+          "typescript", 
+          "python", 
+          "godot", 
+          "github", 
+          "godotengine", 
+          "dotnet",
+          "git",
+          "java",
+          "c",
+          "leagueoflegends",
+          "tailwindcss",
+          "html5",
+          "css3",
+          "javascript",
+          "trello",
+          "jira",
+          "unity",
+          "csharp",
+          "cplusplus",
+          "linux",
+          "dota2",
+          "ubuntu",
+          "flask"
+        ]}></IconCloud>
     </>
   )
 }
